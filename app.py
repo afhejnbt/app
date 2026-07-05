acfaef1a9fc11ae33773c17f5dc22580cf2dabc6
afhejnbt <ba425879@gmail.com>
2026-07-05 04:50:59 +0300

    Remove hardcoded AssemblyAI API key
    Remove hardcoded AssemblyAI API key from app.py.

--- app.py
+++ app.py
@@ -22,8 +22,6 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-ASSEMBLYAI_API_KEY = "5f06039b401740c49a845ab4db2a0421"
-
 try:
     aai.settings.api_key = st.secrets["ASSEMBLYAI_API_KEY"]
 except (KeyError, FileNotFoundError):
